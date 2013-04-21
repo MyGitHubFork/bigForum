411ac0cfcb3b50f60b2f27c435b802357f4c9766
yanhaijing <776771343@qq.com>
2013-04-21 12:21:27 +0800

    update 前台
    1.主页通知列表完成
    2.主页热点推荐完成
    3.通知列表页完成
    4.重构前台页面

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 	 * 主页php
 	 */
 	 include_once('global.php');
+	 include_once 'admin/db_notice.php';
+	$notice = new NoticeModel($db);
+	
+	$result = $notice->get_page(1, 8);
+	
+	$smarty->assign('notices', $result);
 	
 	$smarty->display("index.html");
 ?>
